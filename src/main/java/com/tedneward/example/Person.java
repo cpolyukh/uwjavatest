29a7b597dc2064cd2ef8caad7332f63141c41d0f
cpolyukh <cpolyukh@uw.edu>
2015-04-06 23:02:14 -0700

    Update Person.java

--- src/main/java/com/tedneward/example/Person.java
+++ src/main/java/com/tedneward/example/Person.java
@@ -103,10 +103,8 @@ public class Person implements Comparable<Person> {
     
     if (salaryDifference < 0) {
       return -1;
-    } else if (salaryDifference > 0) {
-      return 1;
     } else {
-      return 0;
+      return 1;
     }
   }
 
